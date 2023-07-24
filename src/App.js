7c656675a7047a4c84524fa3d0e4b83e1c887fdb
briangood35 <brigood@umich.edu>
2023-07-23 21:10:40 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Item from './Item'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Grocery List!</h1>
+      <div className='center'>
+        <div className='Item-Box'>
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Item.js
@@ -0,0 +1,13 @@
+import './item.css'
+
+function Item() {
+    return (
+      <div className='Row'>
+        <p>Item</p>
+        <input type="checkbox" />
+        <button></button>
+      </div>
+    );
+  }
+  
+  export default Item;
