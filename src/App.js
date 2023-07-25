887e8d256334c03f8a62c0aad78df59dbc39bdcd
briangood35 <brigood@umich.edu>
2023-07-24 20:49:57 -0400

    created add-box input and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Item from './Item'
+import AddBox from './addbox'
+import { useState } from 'react'
 
 function App() {
+
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+
   return (
     <div className='App'>
       <h1>Grocery List!</h1>
+      <div className='center'>
+        <div className='Add-Box'>
+          <AddBox />
+        </div>
+      </div>
       <div className='center'>
         <div className='Item-Box'>
           <Item />
---
+++ src/addbox.js
@@ -0,0 +1,12 @@
+import './addbox.css'
+
+function AddBox() {
+    return (
+      <div className='Row'>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+    );
+  }
+  
+  export default AddBox;
